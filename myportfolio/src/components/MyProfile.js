96e7e6ee7e419ccf842987cf7fdcedabf13122df
flgaitan <fiama_gaitan92@hotmail.com>
2023-03-04 19:59:14 -0600

    added cards with new images: and links

--- myportfolio/src/components/MyProfile.js
+++ myportfolio/src/components/MyProfile.js
@@ -14,12 +14,17 @@ function MyProfile() {
             <p>Caption Here</p>
        
         <div className="pcards">
-            <ProfileItem image={Work1} heading="PROJECT 1" text="Caption this"/>
-            <ProfileItem image={Work2} heading="PROJECT 2" text="Caption this"/>
-            <ProfileItem image={Work3} heading="PROJECT 3" text="Caption this"/>
-            <ProfileItem image={Work4} heading="PROJECT 4" text="Caption this"/>
-            <ProfileItem image={Work5} heading="PROJECT 5" text="Caption this"/>
-            <ProfileItem image={Work6} heading="PROJECT 6" text="Caption this"/>
+
+            <ProfileItem image={Work1} heading="PROJECT 1" text="Caption this" github="https://github.com/flgaitan?tab=repositories" heroku="https://github.com/flgaitan?tab=repositories"/>
+            <ProfileItem image={Work2} heading="PROJECT 2" text="Caption this" github="https://github.com/flgaitan?tab=repositories" heroku="https://github.com/flgaitan?tab=repositories"/>
+            <ProfileItem image={Work3} heading="PROJECT 3" text="Caption this" github="https://github.com/flgaitan?tab=repositories" heroku="https://github.com/flgaitan?tab=repositories" />
+          
+        </div>
+        <div className="pcards">
+            <ProfileItem image={Work4} heading="PROJECT 4" text="Caption this" github="https://github.com/flgaitan?tab=repositories" heroku="https://github.com/flgaitan?tab=repositories"/>
+            <ProfileItem image={Work5} heading="PROJECT 5" text="Caption this" github="https://github.com/flgaitan?tab=repositories" heroku="https://github.com/flgaitan?tab=repositories" />
+            <ProfileItem image={Work6} heading="PROJECT 6" text="Caption this" github="https://github.com/flgaitan?tab=repositories" heroku="https://github.com/flgaitan?tab=repositories"/>
+
         </div>
     </div>
     );
--- myportfolio/src/components/ProfileItem.js
+++ myportfolio/src/components/ProfileItem.js
@@ -9,6 +9,21 @@ function ProfileItem(props){
             </div>
             <h4>{props.heading}</h4>
             <p>{props.text}</p>
+            <section className='links'>
+                <section className='github'> 
+                <a href={props.github}>
+                 <h5>GITHUB</h5>    
+                </a> 
+    
+                </section>
+                <section className='web-link'>
+                <a href={props.heroku}>
+                    <h5>WEBSITE</h5>
+                </a> 
+                
+
+                </section>
+            </section>
         </div>
     )
 }
